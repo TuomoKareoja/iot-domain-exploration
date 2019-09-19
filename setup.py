13de66a69da5a1a1e91cc17d76f47d9416b682df
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-09-19 17:49:20 +0200

    initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploratory analysis of electricity use measured with sub-meters. Ubiqum Data Science Bootcamp project.',
+    author='Tuomo Kareoja',
+    license='MIT',
+)
